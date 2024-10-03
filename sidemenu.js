598facb361c16faa821e4067b279f17e445dfdd4
bokoraretawa <finemarch0618@gmail.com>
2024-10-03 16:57:22 +0900

    fontchange
    fontchange

---
+++ sidemenu.js
@@ -0,0 +1,14 @@
+const text =
+`<div class='sidemenu'>
+<nav>
+    <ul>
+        <li><a href='index.html'>トップ</a>
+        <li><a href='bot.html'>botコマンド</a>
+        <li><a href='illust.html'>ぎゃらりー</a>
+        <li><a href='q&a.html'>Q & A<a>
+        <li><a href='thanks.html'>Thanks</a>
+        <li><a href=''>管理者</a>
+    </ul>
+    </nav>
+</div>`;
+document.write(text);
