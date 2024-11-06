9e812be051b46a0900f40fa1c39b9bb9ef9231e9
bokoraretawa <finemarch0618@gmail.com>
2024-11-06 19:39:06 +0900

    commission

--- sidemenu.js
+++ sidemenu.js
@@ -5,7 +5,8 @@ const text =
         <li class="li1"><a href='index.html'>トップ</a>
         <li class="libot"><a href='bot.html'>botコマンド</a>
         <li class="li3"><a href='illust.html'>ぎゃらりー</a>
-        <li class="liqa"><a href='qa.html'>Q&A ・ ご依頼関連<a>
+        <li class="liqa"><a href='commission.html'>ご依頼関連<a>
+        <li class="liqa"><a href='qa.html'>Q&A<a>
         <li class="li5"><a href='thanks.html'>Thanks</a>
         <li class="li6"><a href='admin.html'>管理者</a>
     </ul>
