4c65f96cfbe5a106e3d6116c8e3cca152be30b33
bokoraretawa <finemarch0618@gmail.com>
2024-11-12 19:46:05 +0900

    menu

--- sidemenu.js
+++ sidemenu.js
@@ -2,9 +2,9 @@ const text =
 `<div class='sidemenu'>
     <nav id=sidemenu">
     <ul>
-        <li class="li1"><a href='index.html'>トップ</a>
+        <li class="lihome"><a href='index.html'>トップ</a>
         <li class="libot"><a href='bot.html'>botコマンド</a>
-        <li class="li3"><a href='illust.html'>ぎゃらりー</a>
+        <li class="lipic"><a href='illust.html'>ぎゃらりー</a>
         <li class="liqa"><a href='commission.html'>ご依頼関連<a>
         <li class="liqa"><a href='qa.html'>Q&A<a>
         <li class="li5"><a href='thanks.html'>Thanks</a>
@@ -12,5 +12,5 @@ const text =
     </ul>
     </nav>
 </div>
-<button class="menubutton" style="font-size:20px;">メニューボタン</button>`;
+<button class="menubutton"><img src="img/menu.png" style="width:100%;"></button>`;
 document.write(text);
