d7c35c1717fb1088e23e79459f0256151fdd9be7
bokoraretawa <finemarch0618@gmail.com>
2024-12-08 19:36:57 +0900

    メイン画像周辺、@media、背景色

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,15 +1,30 @@
 const text =
 `<div class='sidemenu'>
     <nav id=sidemenu">
-    <ul>
+    <ul class="sidemenuul">
         <li class="lihome"><a href='index.html'>トップ</a>
         <li class="libot"><a href='bot.html'>botコマンド</a>
         <li class="lipic"><a href='illust.html'>ぎゃらりー</a>
-        <li class="liqa"><a href='commission.html'>ご依頼関連<a>
+        <li class="lico"><a href='commission.html'>ご依頼関連<a>
         <li class="liqa"><a href='qa.html'>Q&A<a>
         <li class="li5"><a href='thanks.html'>Thanks</a>
         <li class="li6"><a href='admin.html'>管理者</a>
     </ul>
+        <a class="sidemenuLinks" href="https://wavebox.me/wave/a6rm4vicaz7he5rm/">質問箱</a><br>
+        <a href="https://clap.fc2.com/post/bokoraretawa/?url=https%3A%2F%2Fbokoraretawa.github.io&title=%E3%81%BC%E3%81%93%E3%81%BC%E3%81%93%E3%81%A1%E3%82%83%E3%82%93%E3%81%AE%E3%83%9B%E3%83%BC%E3%83%A0%E3%83%9A%E3%83%BC%E3%82%B8" target="_blank" title="web拍手 by FC2"><img src="http://clap.fc2.com/images/button/white/bokoraretawa?url=https%3A%2F%2Fbokoraretawa.github.io&amp;lang=ja" alt="web拍手 by FC2" style="border:none;" /></a>
+        <br>
+        <a class="sidemenuLinks"　href="tySup.html">ご支援</a><br>
+        <br>
+
+        <div>
+        <p class="sidemenuLinks" style="color:red;text-decoration:none;">【PR】
+        <d style="color:black">周辺機器のご紹介
+        <br>
+        ・<a class="prURL" href="https://amzn.to/3B5PSS8" target="_blank" rel="noopener noreferrer">XP-Pen Artist16</a><br>
+        ・<a class="prURL" href="https://amzn.to/3ZqXoPB" target="_blank" rel="noopener noreferrer">Blue Yeti X</a><br>
+        ・<a class="prURL" href="https://amzn.to/3BlV4kP" target="_blank" rel="noopener noreferrer">↑の現行品(BM400BK)</a>
+        <d>
+        </p>
     </nav>
 </div>
 <button class="menubutton"><img src="img/menu.png" style="width:100%;"></button>`;
