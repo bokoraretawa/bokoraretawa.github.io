35f20b46d7534175fae9ca61cbaa006ec84dbb5f
bokoraretawa <finemarch0618@gmail.com>
2024-12-15 18:15:19 +0900

    comissionページの見た目改修、TOPの画像追加、フォントクレジット追加

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,21 +1,22 @@
 const text =
 `<div class='sidemenu'>
     <nav id=sidemenu">
-    <ul class="sidemenuul">
-        <li class="lihome"><a href='index.html'>トップ</a>
-        <li class="libot"><a href='bot.html'>botコマンド</a>
-        <li class="lipic"><a href='illust.html'>ぎゃらりー</a>
-        <li class="lico"><a href='commission.html'>ご依頼関連<a>
-        <li class="liqa"><a href='qa.html'>Q&A<a>
-        <li class="li5"><a href='thanks.html'>Thanks</a>
-        <li class="li6"><a href='admin.html'>管理者</a>
-    </ul>
+        <ul class="sidemenuul">
+            <li class="lihome"><a href='index.html'>トップ</a>
+            <li class="libot"><a href='bot.html'>botコマンド</a>
+            <li class="lipic"><a href='illust.html'>ぎゃらりー</a>
+            <li class="lico"><a href='commission.html'>ご依頼関連<a>
+            <li class="liqa"><a href='qa.html'>Q&A<a>
+            <li class="li5"><a href='thanks.html'>Thanks</a>
+            <li class="li6"><a href='admin.html'>管理者</a>
+        </ul>
+        <p class="sidemenuLinks" style="text-decoration:none;">↓ただの拍手です<br>↓押してもらえると喜ぶ</p>
         <a href="https://clap.fc2.com/post/bokoraretawa/?url=https%3A%2F%2Fbokoraretawa.github.io&title=%E3%81%BC%E3%81%93%E3%81%BC%E3%81%93%E3%81%A1%E3%82%83%E3%82%93%E3%81%AE%E3%83%9B%E3%83%BC%E3%83%A0%E3%83%9A%E3%83%BC%E3%82%B8" target="_blank" title="web拍手 by FC2"><img src="http://clap.fc2.com/images/button/white/bokoraretawa?url=https%3A%2F%2Fbokoraretawa.github.io&amp;lang=ja" alt="web拍手 by FC2" style="border:none;" /></a>
         <br>
         <br>
         <a class="sidemenuLinks" href="tySup.html">ご支援フォーム</a><br>
         <br>
-        <a class="sidemenuLinks" href="https://wavebox.me/wave/a6rm4vicaz7he5rm/">質問箱</a><br>
+        <a class="sidemenuLinks" href="https://wavebox.me/wave/a6rm4vicaz7he5rm/" target="_blank" rel="noopener noreferrer">質問箱(外部サイト)</a><br>
         <br>
         <div>
         <p class="sidemenuLinks" style="color:red;text-decoration:none;">【PR】
