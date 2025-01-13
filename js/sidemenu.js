d472a038bc32fcec0d303c44ecd7b1fb97e82d15
bokoraretawa <finemarch0618@gmail.com>
2025-01-13 19:07:19 +0900

    sidemenu,commission update

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,35 +1,48 @@
 const text =
-`<div class='sidemenu'>
-    <nav id=sidemenu">
-        <ul class="sidemenuul">
-            <li class="lihome"><a href='index.html'>トップ</a>
-            <li class="libot"><a href='bot.html'>botコマンド</a>
-            <li class="lipic"><a href='illust.html'>ぎゃらりー</a>
-            <li class="lico"><a href='commission.html'>ご依頼関連<a>
-            <li class="liqa"><a href='rules.html'>チャットルール<a>
-            <li class="li5"><a href='thanks.html'>Thanks</a>
-            <li class="li6"><a href='admin.html'>管理者</a>
-        </ul>
-        <p class="sidemenuLinks" style="text-decoration:none;">↓ただの拍手です<br>↓押してもらえると喜ぶ</p>
-        <a href="https://clap.fc2.com/post/bokoraretawa/?url=https%3A%2F%2Fbokoraretawa.github.io&title=%E3%81%BC%E3%81%93%E3%81%BC%E3%81%93%E3%81%A1%E3%82%83%E3%82%93%E3%81%AE%E3%83%9B%E3%83%BC%E3%83%A0%E3%83%9A%E3%83%BC%E3%82%B8" target="_blank" title="web拍手 by FC2"><img src="http://clap.fc2.com/images/button/white/bokoraretawa?url=https%3A%2F%2Fbokoraretawa.github.io&amp;lang=ja" alt="web拍手 by FC2" style="border:none;" /></a>
-        <br>
-        <br>
-        <a class="sidemenuLinks" href="tySup.html">ご支援フォーム</a><br>
-        <br>
-        <a class="sidemenuLinks" href="https://wavebox.me/wave/a6rm4vicaz7he5rm/" target="_blank" rel="noopener noreferrer">質問箱(外部サイト)</a><br>
-        <br>
-        <br>
-        <div>
+`
+<div class="sidemenu1div">
+    <ul class="sidemenuul">
+        <li class="lihome"><a href='index.html'>トップ</a>
+        <li class="libot"><a href='bot.html'>botコマンド</a>
+        <li class="lipic"><a href='illust.html'>ぎゃらりー</a>
+        <li class="lico"><a href='commission.html'>ご依頼関連<a>
+        <li class="liqa"><a href='rules.html'>ルール・質問<a>
+        <li class="li5"><a href='thanks.html'>Thanks</a>
+        <li class="li6"><a href='admin.html'>管理者</a>
+    </ul>
+    <p class="sidemenuLinks" style="text-decoration:none;">↓ただの拍手です<br>↓押してもらえると喜ぶ</p>
+    <a href="https://clap.fc2.com/post/bokoraretawa/?url=https%3A%2F%2Fbokoraretawa.github.io&title=%E3%81%BC%E3%81%93%E3%81%BC%E3%81%93%E3%81%A1%E3%82%83%E3%82%93%E3%81%AE%E3%83%9B%E3%83%BC%E3%83%A0%E3%83%9A%E3%83%BC%E3%82%B8" target="_blank" title="web拍手 by FC2" ><img src="http://clap.fc2.com/images/button/white/bokoraretawa?url=https%3A%2F%2Fbokoraretawa.github.io&amp;lang=ja" alt="web拍手 by FC2" style="border:none;" / class="webclap"></a>
+    <br>
+    <br>
+    <a class="sidemenuLinks" href="tySup.html">ご支援フォーム</a><br>
+    <br>
+    <a class="sidemenuLinks" href="https://wavebox.me/wave/a6rm4vicaz7he5rm/" target="_blank" rel="noopener noreferrer">質問箱(外部サイト)</a><br>
+    <br>
+    <br>
+    <div>
         <p class="sidemenuLinks" style="color:red;text-decoration:none;">【PR】
-        <d style="color:black">周辺機器のご紹介
-        <p class="sidemenuLinks" style="color:black;text-decoration:none;font-size:15px;" >Amazon のアソシエイトとして、bokoraretawaは適格販売により収入を得ています。</p>
+        <d style="color:black">周辺機器のご紹介<br>
         ・<a class="prURL" href="https://amzn.to/3B5PSS8" target="_blank" rel="noopener noreferrer">XP-Pen Artist16</a><br>
         ・<a class="prURL" href="https://amzn.to/3ZqXoPB" target="_blank" rel="noopener noreferrer">Blue Yeti X</a><br>
         ・<a class="prURL" href="https://amzn.to/3BlV4kP" target="_blank" rel="noopener noreferrer">↑の現行品(BM400BK)</a>
         <d>
         </p>
         <br>
-    </nav>
+    </div>
 </div>
-<button class="menubutton"><img src="img/menu.png" style="width:100%;"></button>`;
+
+<div class='sidemenu2div'>
+    <details id=sidemenu2">
+        <summary>めにゅー</summary>
+        <ul class="sidemenuul">
+            <li class="lihome"><a href='index.html'>トップ</a>
+            <li class="libot"><a href='bot.html'>botコマンド</a>
+            <li class="lipic"><a href='illust.html'>ぎゃらりー</a>
+            <li class="lico"><a href='commission.html'>ご依頼関連<a>
+            <li class="liqa"><a href='rules.html'>ルール・質問<a>
+            <li class="li5"><a href='thanks.html'>Thanks</a>
+            <li class="li6"><a href='admin.html'>管理者</a>
+        </ul>
+    </details>
+</div>`;
 document.write(text);
